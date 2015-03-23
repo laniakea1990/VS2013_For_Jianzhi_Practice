5ecd2c4e248b9f19387fbed8c7d592f78aed1f3d
raiseking <jinranran1990910@163.com>
2015-03-23 10:11:41 +0800

    0323

--- VS2013_JianZhi_Practice/VS2013_JianZhi_Practice.cpp
+++ VS2013_JianZhi_Practice/VS2013_JianZhi_Practice.cpp
@@ -1,11 +1,37 @@
-// VS2013_JianZhi_Practice.cpp : 定义控制台应用程序的入口点。
-//
-
+#include <iostream>
 #include "stdafx.h"
+#include <stdio.h>
+using namespace std;
 
-
-int _tmain(int argc, _TCHAR* argv[])
-{
-	return 0;
+#define N 1002
+int number[N][N];
+int m, n, t;
+int main(){
+	while (scanf_s("%d %d %d", &m, &n, &t) != EOF){
+		for (int i = 0; i < m; ++i){
+			for (int j = 0; j < n; ++j){
+				scanf_s("%d", &number[i][j]);
+			}
+		}
+		int r = 0, c = n - 1;
+		bool found = false;
+		while (r < m && c >= 0){
+			if (number[r][c] == t){
+				found = true;
+				break;
+			}
+			else if (number[r][c] > t){
+				--c;
+			}
+			else {
+				++r;
+			}
+		}
+		if (found){
+			printf("Yes\n");
+		}
+		else{
+			printf("No\n");
+		}
+	}
 }
-
